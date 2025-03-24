1228838ed2e9434fb6dfade2c1fc9c11bf30ce6e
Jurus <jursmarcel555@gmail.com>
2025-03-24 12:19:28 +0100

    dodanie RETURN_VALUE

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,6 +4,7 @@ public class DefaultCountingOutRhymer {
 
     public static int CAPACITY = 12;
     public static int EMPTY_INDEX = -1;
+    public static int RETURN_VALUE = -1;
     private static int FULL_INDEX = CAPACITY-1;
     private final int[] NUMBERS = new int[CAPACITY];
 
@@ -25,13 +26,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return EMPTY_INDEX;
+            return RETURN_VALUE;
         return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return EMPTY_INDEX;
+            return RETURN_VALUE;
         return NUMBERS[total--];
     }
 
