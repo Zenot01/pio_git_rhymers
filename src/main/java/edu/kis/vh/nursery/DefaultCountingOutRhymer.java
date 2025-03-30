668823467e81f4c449cb6b374a1718d4454b45aa
F G <filipkolo50@gmail.com>
2025-03-30 21:14:26 +0200

    9. zastosowanie mozliwie najmniejszej widocznosci

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,9 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static int CAPACITY = 12;
-    public static int EMPTY_INDEX = -1;
-    public static int RETURN_VALUE = -1;
+    private static int CAPACITY = 12;
+    private static int EMPTY_INDEX = -1;
+    private static int RETURN_VALUE = -1;
     private static int FULL_INDEX = CAPACITY-1;
     private final int[] numbers = new int[CAPACITY];
 
